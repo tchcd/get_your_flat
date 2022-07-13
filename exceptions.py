b82048895aa9ed640951feeaa8aab4cfe4e433f4
NP <imbo11@mail.ru>
2022-07-13 13:11:04 +0400

    add exceptions in exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -3,5 +3,14 @@ class MyEx(Exception):
     """Exception"""
     pass
 
+
 class myEx2(Exception):
+    pass
+
+
+class db_connect_fail(Exception):
+    pass
+
+
+class file_write_fail(Exception):
     pass
