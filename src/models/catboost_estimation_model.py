b3c7791a91d179d4a8c9f6dc8362326a83d810d3
NP <imbo11@mail.ru>
2022-07-25 23:35:04 +0400

    src folder refactoring

---
+++ src/models/catboost_estimation_model.py
@@ -0,0 +1,33 @@
+import pickle
+import json
+import pandas as pd
+
+# Для начала нужно из jupyter выгрузить обученную модель кэтбуст
+# и подгружаем здесь
+
+
+last_model_path = "./models/init_model.sav"
+model = pickle.load(open(last_model_path, "rb"))
+cols = model.feature_names_
+
+
+# Здесь функция кэтбуста
+def get_daily_predict(raw_json: json) -> pd.DataFrame:
+    """CatBoost function does predictions for not evaluation dataframe after parsing function
+
+    Args:
+        raw_json (json) : json with data objects
+
+    Returns:
+        pd.DataFrame: with evaluation value. Sending to the database
+
+    """
+
+    df = pd.read_json(raw_json)
+    df["rating"] = model.predict(df[cols])
+    return df
+
+
+if __name__ == "__main__":
+    predict = get_daily_predict("json_prep.json")
+    predict.to_csv(r"C:\Users\q\Desktop\view_w_r.csv")
---
+++ src/models/new_cards_predict.py
@@ -0,0 +1,22 @@
+# здесь мы достаем из БД новые объявления
+# делаем для них предикт
+# добавляем предикты в БД (или перезаливаем в БД объекты)
+
+from src.database import Database
+from catboost_estimation_model import get_daily_predict
+
+# 1. get_cards_from_db - не размеченые объекты из БД.
+# 2. start_catboost - применяет алгоритм к полученным объектам
+# 3. отдает размеченные объекты в функцию миграции из модуля migration?
+
+
+# Не знаю, видимо обернуть это все в функцию predict() и запускать из main
+
+# Получаем из БД не размеченные новые карточки, после парсера
+not_evaluated_cards = Database.get_not_evaluated_cards()
+
+# Применяем CatBoost к неразмеченным
+predicted_cards = get_daily_predict(not_evaluated_cards)
+
+# Заливаем в БД
+Database.add_daily_avito_cards_to_db(predicted_cards)
